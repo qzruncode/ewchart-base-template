3acbe1540e0ce8ad605ae39760035de3a840b7b5
叶家伟 <jiaweiye.yjw@alibaba-inc.com>
2023-02-28 19:59:23 +0800

    feat: 折线图

--- src/code/page1.tsx
+++ src/code/page1.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import EWChart from 'ewchart';
 
 const Test = () => {
-  return <div className="test_box">hello1</div>;
+  return (
+    <div className="test_box">
+      hello1{' '}
+      <EWChart
+        chart={{ type: 'line' }}
+        size={{
+          // 宽度自适应
+          height: 300,
+          top: 20,
+          right: 30,
+          bottom: 30,
+          left: 50,
+        }}
+      />
+    </div>
+  );
 };
 
 export default Test;
