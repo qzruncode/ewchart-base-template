5c0c72f258c7e9c44335b9c068751122e5101267
叶家伟 <jiaweiye.yjw@alibaba-inc.com>
2023-03-09 19:31:51 +0800

    feat: 直方图

--- src/code/histogram.tsx
+++ src/code/histogram.tsx
@@ -9,11 +9,27 @@ const des = `
 const arr = [
   {
     label: 'a1',
-    values: [29649, 31561, 3210, 4723, 5287],
+    values: [29649, 5287],
   },
   {
     label: 'a2',
-    values: [5287, 4723, 3210, 31561, 29649],
+    values: [5287, 5287],
+  },
+  {
+    label: 'a3',
+    values: [4723, 5287],
+  },
+  {
+    label: 'a4',
+    values: [3210, 5287],
+  },
+  {
+    label: 'a5',
+    values: [31561, 5287],
+  },
+  {
+    label: 'a6',
+    values: [29649, 5287],
   },
 ];
 
@@ -26,9 +42,9 @@ const chartSizeParams = {
   left: 30,
 };
 
-const PieChart = () => {
+const HistogramChart = () => {
   const tooltipRef = useRef(null);
-  const [pieData, setPieData] = useState(arr);
+  const [histogramData, setHistogramData] = useState(arr);
   const [labels] = useState(() => arr.map(d => d.label));
   const [chooseIndex, setChooseIndex] = useState<number | undefined>(undefined);
 
@@ -37,68 +53,60 @@ const PieChart = () => {
     data: Array<{ color?: string; label: string; value: number | null }>, // 当前点的信息
     position: { x: number; y: number } // 鼠标的实时位置
   ) => {
-    if (tooltipRef.current) {
-      if (type === 'move') {
-        if (type === 'move') {
-          const maxOffsetX = position.x + tooltipRef.current.offsetWidth + 20;
-          if (maxOffsetX < tooltipRef.current.parentElement.offsetWidth) {
-            tooltipRef.current.style.left = position.x + 20 + 'px';
-          }
+    console.log(type, data, position);
+    // if (tooltipRef.current) {
+    //   if (type === 'move') {
+    //     if (type === 'move') {
+    //       const maxOffsetX = position.x + tooltipRef.current.offsetWidth + 20;
+    //       if (maxOffsetX < tooltipRef.current.parentElement.offsetWidth) {
+    //         tooltipRef.current.style.left = position.x + 20 + 'px';
+    //       }
 
-          tooltipRef.current.style.top = position.y + tooltipRef.current.offsetHeight + 20 + 'px';
-          let html = '';
-          data.forEach(d => {
-            html += `<div className="title"><span>${d.label}</span>：<span>${d.value}</span></div>`;
-          });
-          if (html === '') {
-            tooltipRef.current.style.display = 'none';
-          } else {
-            tooltipRef.current.style.display = 'block';
-            tooltipRef.current.innerHTML = html;
-          }
-        } else if (type === 'enter') {
-          tooltipRef.current.style.display = 'block';
-        } else if (type === 'leave') {
-          tooltipRef.current.style.display = 'none';
-        }
-      } else if (type === 'enter') {
-        tooltipRef.current.style.display = 'block';
-      } else if (type === 'leave') {
-        tooltipRef.current.style.display = 'none';
-      }
-    }
+    //       tooltipRef.current.style.top = position.y + tooltipRef.current.offsetHeight + 20 + 'px';
+    //       let html = '';
+    //       data.forEach(d => {
+    //         html += `<div className="title"><span>${d.label}</span>：<span>${d.value}</span></div>`;
+    //       });
+    //       if (html === '') {
+    //         tooltipRef.current.style.display = 'none';
+    //       } else {
+    //         tooltipRef.current.style.display = 'block';
+    //         tooltipRef.current.innerHTML = html;
+    //       }
+    //     } else if (type === 'enter') {
+    //       tooltipRef.current.style.display = 'block';
+    //     } else if (type === 'leave') {
+    //       tooltipRef.current.style.display = 'none';
+    //     }
+    //   } else if (type === 'enter') {
+    //     tooltipRef.current.style.display = 'block';
+    //   } else if (type === 'leave') {
+    //     tooltipRef.current.style.display = 'none';
+    //   }
+    // }
   };
 
   const handelFootClick = (i: number) => {
     if (i === chooseIndex) {
-      const newPieData = [...pieData];
-      newPieData.forEach(d => {
-        delete d.choose;
-      });
-      setPieData(newPieData);
+      setHistogramData(arr);
       setChooseIndex(undefined);
     } else {
       setChooseIndex(i);
       if (i !== undefined) {
-        const newPieData = [...pieData];
-        newPieData.forEach(d => {
-          delete d.choose;
-        });
-        newPieData[i] = Object.assign({}, newPieData[i], { choose: true });
-        setPieData(newPieData);
+        setHistogramData([arr[i]]);
       }
     }
   };
 
   return (
-    <div className="my-chart">
+    <div className="my-chart" style={{ flex: 1 }}>
       <Button
         onClick={() => {
-          const newData = [...pieData];
+          const newData = [...histogramData];
           newData.forEach(d => {
             d.values = disOrder(d.values);
           });
-          setPieData(newData);
+          setHistogramData(newData);
         }}>
         刷新
       </Button>
@@ -106,18 +114,18 @@ const PieChart = () => {
         type="histogram"
         size={chartSizeParams}
         data={{
-          groups: pieData,
+          groups: histogramData,
           y: {
             start: 0,
             end: 40000,
           },
           yUnit: 'K',
           gap: 5, // 柱子之间的间隙
-          maxWidth: 50, // 柱子最大的宽度
+          maxWidth: 30, // 柱子最大的宽度
         }}
-        // method={{
-        //   onMove: handleMove,
-        // }}
+        method={{
+          onMove: handleMove,
+        }}
         // interactive={{
         //   mouse: {
         //     shadow: true, // 是否展示选中pie的阴影
@@ -148,7 +156,10 @@ const PieChart = () => {
 const Tooltip = () => {
   return (
     <div className="test_box">
-      <PieChart />
+      <div style={{ display: 'flex' }}>
+        <HistogramChart />
+        <HistogramChart />
+      </div>
       <pre>
         <code>{des}</code>
       </pre>
