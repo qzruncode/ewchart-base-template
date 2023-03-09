af888a0af384a44393534d0a41722ef418c96b98
叶家伟 <jiaweiye.yjw@alibaba-inc.com>
2023-03-09 15:35:28 +0800

    feat: 饼图

--- src/code/pie.tsx
+++ src/code/pie.tsx
@@ -1,14 +1,26 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import EWChart from 'ewchart';
-import { Breadcrumb, Button } from 'antd';
+import React, { useRef, useState } from 'react';
+import EWChart, { getColorsByIndex } from 'ewchart';
+import { Button } from 'antd';
 import { disOrder } from './helper';
 
 const des = `
-1. 只需要改type为arealine，其他操作同折线图
-  <EWChart
-    type="arealine"
-    ...
-  />
+示例代码，具体请查看此demo源码
+<EWChart
+  type="pie"
+  size={chartSizeParams}
+  data={{
+    groups: pieData,
+  }}
+  method={{
+    onMove: handleMove,
+  }}
+  interactive={{
+    mouse: {
+      shadow: true, // 是否展示选中pie的阴影
+      pieText: true, // 是否展示选中pie的提示文字
+    },
+  }}
+/>
 `;
 
 const arr = [
@@ -44,14 +56,67 @@ const chartSizeParams = {
 };
 
 const PieChart = () => {
+  const tooltipRef = useRef(null);
   const [pieData, setPieData] = useState(arr);
+  const [labels] = useState(() => arr.map(d => d.label));
+  const [chooseIndex, setChooseIndex] = useState<number | undefined>(undefined);
 
   const handleMove = (
     type: 'enter' | 'move' | 'leave', // 鼠标事件类型
     data: Array<{ color?: string; label: string; value: number | null }>, // 当前点的信息
     position: { x: number; y: number } // 鼠标的实时位置
   ) => {
-    console.log(type, data, position);
+    if (tooltipRef.current) {
+      if (type === 'move') {
+        if (type === 'move') {
+          const maxOffsetX = position.x + tooltipRef.current.offsetWidth + 20;
+          if (maxOffsetX < tooltipRef.current.parentElement.offsetWidth) {
+            tooltipRef.current.style.left = position.x + 20 + 'px';
+          }
+
+          tooltipRef.current.style.top = position.y + tooltipRef.current.offsetHeight + 20 + 'px';
+          let html = '';
+          data.forEach(d => {
+            html += `<div className="title"><span>${d.label}</span>：<span>${d.value}</span></div>`;
+          });
+          if (html === '') {
+            tooltipRef.current.style.display = 'none';
+          } else {
+            tooltipRef.current.style.display = 'block';
+            tooltipRef.current.innerHTML = html;
+          }
+        } else if (type === 'enter') {
+          tooltipRef.current.style.display = 'block';
+        } else if (type === 'leave') {
+          tooltipRef.current.style.display = 'none';
+        }
+      } else if (type === 'enter') {
+        tooltipRef.current.style.display = 'block';
+      } else if (type === 'leave') {
+        tooltipRef.current.style.display = 'none';
+      }
+    }
+  };
+
+  const handelFootClick = (i: number) => {
+    if (i === chooseIndex) {
+      const newPieData = [...pieData];
+      newPieData.forEach(d => {
+        delete d.choose;
+      });
+      setPieData(newPieData);
+      setChooseIndex(undefined);
+    } else {
+      setChooseIndex(i);
+      if (i !== undefined) {
+        const newPieData = [...pieData];
+        newPieData.forEach(d => {
+          delete d.choose;
+        });
+        newPieData[i] = Object.assign({}, newPieData[i], { choose: true });
+        setPieData(newPieData);
+      }
+    }
   };
 
   return (
@@ -71,7 +136,29 @@ const PieChart = () => {
         method={{
           onMove: handleMove,
         }}
+        interactive={{
+          mouse: {
+            shadow: true, // 是否展示选中pie的阴影
+            pieText: true, // 是否展示选中pie的提示文字
+          },
+        }}
       />
+
+      <div className="chart-tooltip" ref={tooltipRef}></div>
+
+      <div className="chart-foot">
+        {labels.map((label, index) => {
+          return (
+            <div
+              className={'foot-item ' + (chooseIndex === index || chooseIndex === undefined ? 'choosed' : 'no_choose')}
+              key={index}
+              onClick={() => handelFootClick(index)}>
+              <span className="foot-dot" style={{ backgroundColor: getColorsByIndex(index) }}></span>
+              <span className="foot-label">{label}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
