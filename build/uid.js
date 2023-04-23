e6bdd3e5fad6a553cab046d1520d62cf03af4b24
叶家伟 <jiaweiye.yjw@alibaba-inc.com>
2023-04-23 18:11:39 +0800

    feat: 添加canvas

--- build/uid.js
+++ build/uid.js
@@ -2,5 +2,5 @@ const subAppName = 'darkTunnelBaseTemplate'; // 此子应用的名称
 const hasMaster = !!(window.appState && window.appState[subAppName]);
 
 if (hasMaster) {
-  window.appState[subAppName].uid = '1682244125403y8v7nacuhll';
+  window.appState[subAppName].uid = '1682244628722todly5z84w';
 }
