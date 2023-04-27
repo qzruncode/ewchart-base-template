2f0049a8973519e8529fea4b600d6b85468b5a08
叶家伟 <jiaweiye.yjw@alibaba-inc.com>
2023-04-27 17:53:30 +0800

    fix: 升级依赖包版本

--- build/uid.js
+++ build/uid.js
@@ -2,5 +2,5 @@ const subAppName = 'darkTunnelBaseTemplate'; // 此子应用的名称
 const hasMaster = !!(window.appState && window.appState[subAppName]);
 
 if (hasMaster) {
-  window.appState[subAppName].uid = '16825669659797b8truwpcs9';
+  window.appState[subAppName].uid = '16825891830049jffegpi7ku';
 }
