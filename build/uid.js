2b75c8355837284951f3a2c13b00d5af730e78e6
叶家伟 <jiaweiye.yjw@alibaba-inc.com>
2023-06-01 19:22:06 +0800

    feat: 添加蜡烛图

--- build/uid.js
+++ build/uid.js
@@ -2,5 +2,5 @@ const subAppName = 'darkTunnelBaseTemplate'; // 此子应用的名称
 const hasMaster = !!(window.appState && window.appState[subAppName]);
 
 if (hasMaster) {
-  window.appState[subAppName].uid = '1685618262512tu9k7p45l8i';
+  window.appState[subAppName].uid = '1685618498033xg0vmzxita';
 }
