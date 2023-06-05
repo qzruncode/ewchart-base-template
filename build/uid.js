1fb6645cfc400218085bffbc63aa04a3812e71c1
叶家伟 <jiaweiye.yjw@alibaba-inc.com>
2023-06-05 19:19:51 +0800

    fix: 修复蜡烛图bug

--- build/uid.js
+++ build/uid.js
@@ -2,5 +2,5 @@ const subAppName = 'darkTunnelBaseTemplate'; // 此子应用的名称
 const hasMaster = !!(window.appState && window.appState[subAppName]);
 
 if (hasMaster) {
-  window.appState[subAppName].uid = '1685618498033xg0vmzxita';
+  window.appState[subAppName].uid = '1685963963068b4a3vw3ibgt';
 }
